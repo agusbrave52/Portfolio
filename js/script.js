bed512fad1ca7117e5e6fcb459537938348b009e
agusbrave52 <urubravo52@gmail.com>
2023-01-13 15:05:11 -0300

    cambios 0.6
    cambios de animaciones, efectos y agregamos proyectos

--- js/script.js
+++ js/script.js
@@ -1,35 +1,36 @@
 function clickSobremi(){
     $("#conocimientos").fadeOut(1, function(){
         $("#contacto").fadeOut(1,function(){
-            $("#sobremi").slideToggle(800);
+            $("#proyectos").fadeOut(1, function(){
+                $("#sobremi").slideToggle(800);
+            });
         });
     });
 }
 function clickConocimientos(){
     $("#sobremi").fadeOut(1, function(){
         $("#contacto").fadeOut(1, function(){
-            $("#conocimientos").slideToggle(800);
-        })
+            $("#proyectos").fadeOut(1, function(){
+                $("#conocimientos").slideToggle(800);
+            });
+        });
     });
 }
 function clickContacto(){
     $("#conocimientos").fadeOut(1, function(){
         $("#sobremi").fadeOut(1, function(){
-            $("#contacto").slideToggle(800);
-        })
+            $("#proyectos").fadeOut(1, function(){
+                $("#contacto").slideToggle(800);
+            });
+        });
     });
 }
-// function clickSorteos(){
-//     $("#listReservar").fadeOut(0, function(){
-//         $("#listado").fadeOut(0, function(){
-//             $("#listSorteos").fadeIn(250);
-//         })
-//     })
-// }
-// function clickReservar(){
-//     $("#listSorteos").fadeOut(0, function(){
-//         $("#listado").fadeOut(0, function(){
-//             $("#listReservar").fadeIn(250);
-//         })
-//     })
-// }
+function clickProyectos(){
+    $("#conocimientos").fadeOut(1, function(){
+        $("#sobremi").fadeOut(1, function(){
+            $("#contacto").fadeOut(1, function(){
+                $("#proyectos").slideToggle(800);
+            });
+        });
+    });
+}
